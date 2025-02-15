596ec233c32963f26a958b122f3bcdc0d891f3c0
huishilhs <87135201+huishilhs@users.noreply.github.com>
2025-02-15 18:49:56 +0000

    configure status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { LogBox } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-    <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-  </Stack>;
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <StatusBar style='light'/>
+        <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </GestureHandlerRootView>
+  );
 }
